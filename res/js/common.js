e4ac5c7650ed85884cb259d29f1f29d56c914c05
Changho YUN <dbckgh8010@naver.com>
2024-07-17 15:30:51 +0900

    finish

--- res/js/common.js
+++ res/js/common.js
@@ -14,3 +14,53 @@ $(function() {
         $header.removeClass("on");
     });
 });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 0){
+        $('.header').addClass('fixed')
+    } else {
+        $('.header').removeClass('fixed')
+    };
+});
+
+$('.mo__menu').ready(function() {
+    $('.mo__title').on('click', function() {
+        if($(this).hasClass('on')) {
+            $(this).removeClass('on').next().slideUp(200);
+        } else {
+            slideUp();
+            $(this).addClass('on').next().slideDown(200);
+        }
+        function slideUp() {
+            $('.mo__title').removeClass('on').next().slideUp(200);
+        };
+    });
+});
+
+$(function() {
+    $(".mo__btn").on('click', function(){
+        if ('block' == $(".header__mo").css('display')){
+            $(this).removeClass('on')
+            $(".header__mo").removeClass('on').hide()
+            $('html').css({'overflow-y' : '', 'margin-right' : '0', 'width' : '100%'});
+        } else {
+            $('html').css({'overflow-y' : 'hidden', 'width' : 'auto'});
+            $(this).addClass('on')
+            $(".header__mo").addClass('on').show()
+        }
+    })
+});
+
+$('.mo__sub').ready(function() {
+    $('.list1').on('click', function() {
+        if($(this).hasClass('click')) {
+            $(this).removeClass('click').next().slideUp(200);
+        } else {
+            slideUp();
+            $(this).addClass('click').next().slideDown(200);
+        }
+        function slideUp() {
+            $('.list1').removeClass('click').next().slideUp(200);
+        };
+    });
+}); 
